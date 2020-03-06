25efa5233ddd0d862f87d6c09ae822d1fc0385f7
s4kc0d3 <isaac.iborra@gmail.com>
2020-03-06 09:26:50 +0100

    añado nuevas librerias en settings.py

--- blogcocina/settings.py
+++ blogcocina/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'postres',
+    'clear_cache',
+    'bootstrap4',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [

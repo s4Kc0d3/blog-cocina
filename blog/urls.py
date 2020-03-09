b643ca6b25d4a3ed1bb6464c1793a8a6dbcb2be5
s4kc0d3 <iiborra@externos.itnow.es>
2020-03-09 15:10:47 +0100

    Creo el enlace de añadir posts.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='index'),
+    path('post/new', views.post_new, name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/index.html',{'posts': posts})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('index')
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/post_new.html', {'form': form})

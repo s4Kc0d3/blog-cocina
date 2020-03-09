06a9c270cf5a7a89cb92cf1f8720d972dfe55b6e
fenix15100 <francisco.camacho15@gmail.com>
2020-03-09 12:24:26 +0100

    fix to deploy with heroku

--- blogcocina/settings.py
+++ blogcocina/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,4 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+#FIX DEPLOY HEROKU
+django_heroku.settings(locals())
